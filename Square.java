68b4c0fb11494abefe1d97b992da5da4c19210ae
Abnormal-Jester <googleplex_unknown@yahoo.com>
2015-10-18 13:21:58 -0400

    use FullSquare
    use FullSquare
    
    incorporate FullSquare in the code.

--- Square.java
+++ Square.java
@@ -1,21 +1,29 @@
 public class Square {
-   
-   // ----------
-   // fields
-   // ----------
-   
-   
-   // ----------
-   // constructors
-   // ----------
-   
-   
-   // ----------
-   // methods
-   // ----------
-   
-    public void printMethod() {
-        System.out.println("Printed in Superclass.");
-    }
-    
+	
+	// ----------
+	// fields
+	// ----------
+
+	final int xSquare;
+	final int ySquare;
+
+	// ----------
+	// constructors
+	// ----------
+
+	public Square (int setX, int setY) {
+		xSquare = setX;
+		ySquare = setY;
+	}
+
+
+	// ----------
+	// methods
+	// ----------
+
+	public void printMethod() {
+		System.out.println("Printed in Square.");
+		System.out.println(xSquare + ", " + ySquare);
+	}
+
 }
