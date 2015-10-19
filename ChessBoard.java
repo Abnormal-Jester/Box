d1a9bc86d332dd5070e8901ceaf4cc0bc9becad3
Jarett Lee <googleplex_unknown@yahoo.com>
2015-10-19 14:35:00 -0400

    Add method printBoard
    Add method printBoard

--- ChessBoard.java
+++ ChessBoard.java
@@ -13,8 +13,47 @@ public class ChessBoard {
 	// methods
 	// ----------
 
-	public static void swapSquares () {
+	// Method printBoard
 
+	/*
+
+	(0, 2) (1, 2) (2, 2)
+	(0, 1) (1, 1) (2, 1)
+	(0, 0) (1, 0) (2, 0)
+
+	square[0][2] square[1][2] square[2][2]
+	square[0][1] square[1][1] square[2][1]
+	square[0][0] square[1][0] square[2][0]
+
+	*/
+
+	public static void printBoard (FullSquare[][] printObjectA) {
+		// side line and all square
+		for (int j = printObjectA.length-1; j >= 0; j--) {
+			System.out.print((char) ('a' + j));
+			System.out.print(" ||");
+			for (int i = 0; i <= printObjectA[0].length-1; i++) {
+				System.out.print(' ');
+				printObjectA[i][j].getSquare();
+			}
+			System.out.println();
+		}
+
+		// bottom line
+		System.out.print("    ");
+		for (int i = 0; i <= printObjectA[0].length-1; i++) {
+			System.out.print("==");
+		}
+		System.out.print('=');
+		System.out.println();
+
+		// bottom label
+		System.out.print("    ");
+		for (int i = 0; i <= printObjectA[0].length-1; i++) {
+			System.out.print(' ');
+			System.out.print(i + 1);
+		}
+		System.out.println();
 	}
 
 
@@ -29,40 +68,13 @@ public class ChessBoard {
 		// setting up the ChessBoard
 		FullSquare[][] squareObject = new FullSquare[boardHeight][boardWidth];
 
-		for (int i = 0; i <= boardHeight-1; i++) {
-			for (int j = boardWidth-1; j >= 0; j--) {
+		for (int j = 0; j <= boardHeight-1; j++) {
+			for (int i = 0; i <= boardWidth-1; i++) {
 				squareObject[i][j] = new FullSquare(-1, i, j);
 			}
 		}
 
-		/*
-
-		i, j
-		(0, 2) (1, 2) (2, 2)
-		(0, 1) (1, 1) (2, 1)
-		(0, 0) (1, 0) (2, 0)
-
-		*/
-
-		// side line and
-		for (int i = 0; i <= squareObject.length-1; i++) {
-			System.out.print("||");
-			for (int j = 0; j <= squareObject[0].length-1; j++) {
-				System.out.print(' ');
-				squareObject[i][j].getSquare();
-			}
-			System.out.println();
-		}
-
-		// bottom line
-		System.out.print("  ");
-		for (int i = 0; i <= squareObject[0].length-1; i++) {
-			System.out.print("==");
-		}
-		System.out.print("=");
-
-		System.out.println();
-
+		printBoard(squareObject);
 
 	}
 }
